99ae3f7cdcace4433a4e52f6530fbf046bf11b3e
Ria Talsania <riatalsania0510@gmail.com>
2026-07-23 22:29:52 +0530

    Include ONNX binaries in Vercel bundle

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['onnxruntime-node', 'sharp'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./node_modules/onnxruntime-node/bin/napi-v3/linux/x64/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
